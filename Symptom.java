0aa997219370f44dcb0abfdcbe1b34b12c623730
onurrtan <onur.tan@windowslive.com>
2013-12-15 18:26:46 +0200

    Update and Creation of Symptom Class

--- Symptom.java
+++ Symptom.java
@@ -1,7 +1,32 @@
-/**
- * Created by turkmen on 15.12.2013.
- */
+/* Onur TAN
+
+*/
+
 public class Symptom {
 
 
-}
+    private String cost; 
+    private String infectivity; 
+    private String lethality; 
+
+
+    void changeSymptom(){
+
+    }
+    protected void fillArray(){
+
+    }
+
+
+
+    public String getCost(){
+        return this.cost;
+    }
+
+    public String getInfectivity(){
+        return this.infectivity;
+    }
+
+    public String getLethality(){
+        return this.lethality;
+    }
