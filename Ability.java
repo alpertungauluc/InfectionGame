5c1419aba4b09c4bc2cfbdb0aa6ecc9e205f200e
onurrtan <onur.tan@windowslive.com>
2013-12-15 18:29:19 +0200

    Update Ability.java

--- Ability.java
+++ Ability.java
@@ -1,5 +1,37 @@
 /**
- * Created by turkmen on 15.12.2013.
+ * Created by Batıkan TÜRKMEN and Onur TAN.
  */
 public class Ability {
+
+
+ 
+ 
+    private String coldness; 
+    private double cost; 
+    private String drug;
+    private String temperature; 
+    
+    void changeAbility(){
+
+    }
+    protected void fillArray(){
+
+    }
+
+    public String getColdness(){
+        return this.coldness;
+    }
+    
+    public double getCost(){
+        return this.cost;
+    }
+
+    public String getDrug(){
+        return this.drug;
+    }
+
+    public String getTemperature(){
+        return this.temperature;
+    }
+
 }
